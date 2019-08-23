1ce748b7beb1254b3d07c78584eae088f2f4ca63
AndyBrizhak <andrewbrizhak@gmail.com>
2019-08-24 05:33:39 +0800

    +

--- DI.AutofacLibrary/Program.cs
+++ DI.AutofacLibrary/Program.cs
@@ -1,24 +1,5 @@
-﻿using System;
-
-namespace DI.AutofacLibrary
+﻿namespace DI.AutofacLibrary
 {
-  public class Engine
-  {
-    private ILog log;
-    private int id;
-
-    public Engine(ILog log)
-    {
-      this.log = log;
-      id = new Random().Next();
-    }
-
-    public void Ahead(int power)
-    {
-      log.Write($"Engine [{id}] ahead {power}");
-    }
-  }
-
   public class Car
   {
     private Engine engine;
