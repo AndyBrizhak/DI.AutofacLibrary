844555d3766790c535df5985818edb33f431697a
AndyBrizhak <andrewbrizhak@gmail.com>
2019-08-25 11:08:02 +0800

    +

---
+++ DI.AutofacLibrary/Car.cs
@@ -0,0 +1,20 @@
+namespace DI.AutofacLibrary
+{
+    public class Car
+    {
+        private Engine engine;
+        private ILog log;
+
+        public Car(Engine engine, ILog log)
+        {
+            this.engine = engine;
+            this.log = log;
+        }
+
+        public void Go()
+        {
+            engine.Ahead(100);
+            log.Write("Car going forward...");
+        }
+    }
+}
