9656ea6ecbb9a850c6f7a4b1fd3dba8607cbf716
AndyBrizhak <andrewbrizhak@gmail.com>
2019-08-26 17:12:25 +0800

    +

--- DI.AutofacLibrary/ILog.cs
+++ DI.AutofacLibrary/ILog.cs
@@ -2,6 +2,10 @@ namespace DI.AutofacLibrary
 {
     public interface ILog
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
         void Write(string message);
     }
 }
--- DI.AutofacLibrary/Program.cs
+++ DI.AutofacLibrary/Program.cs
@@ -1,23 +1,5 @@
 ﻿namespace DI.AutofacLibrary
 {
-  public class Car
-  {
-    private Engine engine;
-    private ILog log;
-
-    public Car(Engine engine, ILog log)
-    {
-      this.engine = engine;
-      this.log = log;
-    }
-
-    public void Go()
-    {
-      engine.Ahead(100);
-      log.Write("Car going forward...");
-    }
-  }
-
   internal class Program
   {
     public static void Main(string[] args)
