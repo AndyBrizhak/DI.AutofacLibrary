8b26503ba3414791c05199f5e24be253b6f74cfc
AndyBrizhak <andrewbrizhak@gmail.com>
2019-08-27 14:39:11 +0800

    ++

--- DI.AutofacLibrary/ILog.cs
+++ DI.AutofacLibrary/ILog.cs
@@ -3,7 +3,7 @@ namespace DI.AutofacLibrary
     public interface ILog
     {
         /// <summary>
-        /// 
+        /// ++
         /// </summary>
         /// <param name="message"></param>
         void Write(string message);
